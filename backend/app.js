8eaaed6ac5eb4036e7b9d2fe3ce3f41209956f54
Raj Vishwakarma <raj.vishwakarma123@gmail.com>
2026-01-12 23:54:26 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-     origin:  "http://localhost:5173",
+     origin:  ["https://task-management-dashboard-1-t7i3.onrender.com","http://localhost:5173"],
      methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 })
